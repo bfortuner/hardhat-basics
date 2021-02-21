ba406a02e9841ddb314d78eac2e61b8ee12e241c
Brendan Fortuner <bfortuner@gmail.com>
2021-02-20 18:28:39 -0800

    Init repo

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+require("@nomiclabs/hardhat-ganache");
+require("@nomiclabs/hardhat-waffle");
+
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
+};
